9a0420bde074c134d1ce9bfb0e998544f0afdf43
ahmettaydinn <birceste@gmail.com>
2023-12-20 18:13:10 +0300

    install data fetching packages

---
+++ src/types/service.ts
@@ -0,0 +1,12 @@
+export interface airportInfo {
+  name: string;
+  city: string;
+  country: string;
+  iata_code: string;
+  _geoloc: {
+    lat: number;
+    lng: number;
+  };
+  links_count: number;
+  id: string;
+}
